3e379e5a0e633ea60de052ded7ad00be82eeb54e
kagiso <kagisojamesthomba@gmail.com>
2021-09-30 18:24:37 +0200

    finished user model

---
+++ users/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """Custom User model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "english"
+    LANGUAGE_SETSWANA = "setswana"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_SETSWANA, "Setswana"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_BWP = "bwp"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_BWP, "BWP"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthDate = models.DateField(null=True, blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=10, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
